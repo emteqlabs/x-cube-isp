e946bf6236fd2d5329a856618ab431a21ca9f63a
Bojan Sofronievski <bojan.drago@gmail.com>
2025-07-01 12:08:56 +0200

    Update OV2740 ISP tuning params

--- Projects/DCMIPP_ISP/ISP_IQTune_App/STM32N6570-DK/Inc/isp_param_conf.h
+++ Projects/DCMIPP_ISP/ISP_IQTune_App/STM32N6570-DK/Inc/isp_param_conf.h
@@ -296,93 +296,94 @@ static const ISP_IQParamTypeDef ISP_IQParamCacheInit_OV02C = {
 };
 
 static const ISP_IQParamTypeDef ISP_IQParamCacheInit_OV2740 = {
-    .sensorGainStatic = {
-        .gain = 0,
-    },
-    .sensorExposureStatic = {
-        .exposure = 0,
-    },
-    .AECAlgo = {
-        .enable = 1,
-        .exposureCompensation = EXPOSURE_TARGET_0_0_EV,
-    },
-    .statRemoval = {
-        .enable = 0,
-        .nbHeadLines = 0,
-        .nbValidLines = 0,
-    },
-    .badPixelStatic = {
-        .enable = 0,
-        .strength = 0,
-    },
-    .badPixelAlgo = {
-        .enable = 0,
-        .threshold = 0,
-    },
-    .blackLevelStatic = {
-        .enable = 1,
-        .BLCR = 16,
-        .BLCG = 16,
-        .BLCB = 16,
-    },
-    .demosaicing = {
-        .enable = 1,
-        .type = ISP_DEMOS_TYPE_GRBG,
-        .peak = 2,
-        .lineV = 4,
-        .lineH = 4,
-        .edge = 6,
-    },
-    .ispGainStatic = {
-        .enable = 0,
-        .ispGainR = 0,
-        .ispGainG = 0,
-        .ispGainB = 0,
-    },
-    .colorConvStatic = {
-        .enable = 0,
-        .coeff = { { 0, 0, 0, }, { 0, 0, 0, }, { 0, 0, 0, }, }
-    },
-    .AWBAlgo = {
-        .enable = 1,
-        .id = { "MiniLBox A", "MiniLBox TL84", "MiniLBox D65", "", "", },
-        .referenceColorTemp = { 2665, 3750, 6140, 0, 0, },
-        .ispGainR = { 76000000, 95000000, 131000000, 0, 0, },
-        .ispGainG = { 100000000, 100000000, 100000000, 0, 0, },
-        .ispGainB = { 218000000, 176000000, 146000000, 0, 0, },
-        .coeff = {
-            { { 157610000, 24710000, -88570000, }, { -65040000, 228120000, -32820000, }, { -26150000, -64259999, 325760000, }, },
-            { { 178910000, 5470000, -59790000, }, { -56100000, 225870000, -36100000, }, { -12980000, -55660000, 262750000, }, },
-            { { 188870000, -8850000, -41010000, }, { -42130000, 203070000, -37570000, }, { -6360000, -50460000, 216580000, }, },
-            { { 0, 0, 0, }, { 0, 0, 0, }, { 0, 0, 0, }, },
-            { { 0, 0, 0, }, { 0, 0, 0, }, { 0, 0, 0, }, },
-        },
-    },
-    .contrast = {
-        .enable = 0,
-        .coeff.LUM_0 = 0,
-        .coeff.LUM_32 = 0,
-        .coeff.LUM_64 = 0,
-        .coeff.LUM_96 = 0,
-        .coeff.LUM_128 = 0,
-        .coeff.LUM_160 = 0,
-        .coeff.LUM_192 = 0,
-        .coeff.LUM_224 = 0,
-        .coeff.LUM_256 = 0,
-    },
-    .statAreaStatic = {
-        .X0 = 0,
-        .Y0 = 0,
-        .XSize = 1920,
-        .YSize = 1080,
-    },
-    .gamma = {
-        .enablePipe1 = 1,
-        .enablePipe2 = 1,
-    },
-    .sensorDelay = {
-        .delay = 3,
-    },
+	.sensorGainStatic = {
+			.gain = 0,
+	},
+	.sensorExposureStatic = {
+		.exposure = 5283,
+	},
+	.AECAlgo = {
+		.enable = 1,
+		.exposureCompensation = EXPOSURE_TARGET_0_0_EV,
+		.antiFlickerFreq = 0,
+	},
+	.statRemoval = {
+		.enable = 0,
+		.nbHeadLines = 0,
+		.nbValidLines = 0,
+	},
+	.badPixelStatic = {
+		.enable = 0,
+		.strength = 0,
+	},
+	.badPixelAlgo = {
+		.enable = 0,
+		.threshold = 0,
+	},
+	.blackLevelStatic = {
+		.enable = 1,
+		.BLCR = 4,
+		.BLCG = 4,
+		.BLCB = 4,
+	},
+	.demosaicing = {
+		.enable = 1,
+		.type = ISP_DEMOS_TYPE_GRBG,
+		.peak = 2,
+		.lineV = 4,
+		.lineH = 4,
+		.edge = 6,
+	},
+	.ispGainStatic = {
+		.enable = 1,
+		.ispGainR = 159375000,
+		.ispGainG = 100000000,
+		.ispGainB = 156250000,
+	},
+	.colorConvStatic = {
+		.enable = 1,
+		.coeff = { { 184765625, -8203125, -76171875, }, { -51953125, 215625000, -63671875, }, { -10937500, -92187500, 203515625, }, }
+	},
+	.AWBAlgo = {
+		.enable = 1,
+		.id = { "DIY-LB-2500", "DIY-LB-3500", "DIY-LB-6500", "Free Slot", "Free Slot", },
+		.referenceColorTemp = { 2500, 3500, 6500, 0, 0, },
+		.ispGainR = { 100000000, 128000000, 160000000, 0, 0, },
+		.ispGainG = { 101000000, 100000000, 100000000, 0, 0, },
+		.ispGainB = { 260000000, 187000000, 157000000, 0, 0, },
+		.coeff = {
+			{ { 148530000, 72230000, -120760000, }, { -70590000, 223159999, -52569999, }, { -36110000, -165270000, 301370000, }, },
+			{ { 172200000, 15910000, -88110000, }, { -62020000, 222940000, -60920000, }, { -18090000, -104950000, 223050000, }, },
+			{ { 184820000, -8560000, -76260000, }, { -52220000, 215950000, -63730000, }, { -11240000, -92550000, 203779999, }, },
+			{ { 0, 0, 0, }, { 0, 0, 0, }, { 0, 0, 0, }, },
+			{ { 0, 0, 0, }, { 0, 0, 0, }, { 0, 0, 0, }, },
+		},
+	},
+	.contrast = {
+		.enable = 0,
+		.coeff.LUM_0 = 0,
+		.coeff.LUM_32 = 0,
+		.coeff.LUM_64 = 0,
+		.coeff.LUM_96 = 0,
+		.coeff.LUM_128 = 0,
+		.coeff.LUM_160 = 0,
+		.coeff.LUM_192 = 0,
+		.coeff.LUM_224 = 0,
+		.coeff.LUM_256 = 0,
+	},
+	.statAreaStatic = {
+		.X0 = 483,
+		.Y0 = 287,
+		.XSize = 954,
+		.YSize = 622,
+	},
+	.gamma = {
+		.enablePipe1 = 1,
+		.enablePipe2 = 1,
+	},
+	.sensorDelay = {
+		.delay = 3,
+	},
 };
 
 static const ISP_IQParamTypeDef* ISP_IQParamCacheInit[] = {
